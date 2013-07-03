6991e50e7b6d8ea6702d84e45907fa94f47785d5
Anton Shuleshov <kaasnake@gmail.com>
2013-07-03 16:32:04 +0300

    Disabling logging

--- src/net/kaasnake/Logger.java
+++ src/net/kaasnake/Logger.java
@@ -4,8 +4,8 @@ import android.util.Log;
 
 public class Logger {
 
-	private static final boolean logEnabled = true;
-	private static final String prefix = "C8960LN";
+	private static final boolean logEnabled = false;
+	private static final String prefix = "C8690LN";
 	public static void Log(String suffix, String msg, String mode){
 		if (Logger.logEnabled){
 			if (mode.equals("v")){
